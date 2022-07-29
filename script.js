e633e8febb7c1ebe572a9392032f2f49cf4ee27f
rakhymova <98755830+rakhymova@users.noreply.github.com>
2022-07-29 17:33:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+const body = document.body;
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("nonscroll")
+  renderPopup();
+}
+function renderPopup() {
+  popup.appendChild(menu);
+}
